c0b5a93d1b59ce38c89d714d2fc91372cdd81349
Stefan Kowal <kowalmax.s@gmail.com>
2020-03-26 17:20:34 -0400

    port changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-const port = 8765;
+const port = process.env.PORT || 8765;
 app.listen(port, () => {
   console.log(`listening http://localhost:${port}`);
 });
