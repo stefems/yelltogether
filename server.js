d373c4a940312a28050c93bd93d2660b7d3cddc7
Stefan Kowal <kowalmax.s@gmail.com>
2020-04-09 15:21:32 -0400

    using socket io

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const path = require('path');
 const fetch = require("node-fetch");
-const app = express();
+const socketIO = require('socket.io');
 const WebSocket = require('ws');
 var fs = require('fs');
 var files = fs.readdirSync('screams/');
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
 const port = process.env.PORT || 8765;
-app.listen(port, () => {
+
+const server = express()
+.use(express.static('public'))
+.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+})
+.listen(port, () => {
   console.log(`listening http://localhost:${port}`);
 });
-const socketServer = new WebSocket.Server();
+const socketServer = socketIO(server);
+
 
 const url = "https://api.giphy.com/v1/gifs/random?api_key=Qv3h2IWU87lkdBEy7C7Gw4JCw5BkGZF0&tag=shout";
 const getGif = async () => {
@@ -25,41 +28,20 @@ const getGif = async () => {
       console.log(error);
     }
 };
-  
 socketServer.on('connection', (socketClient) => {
-
+  socketClient.on('disconnect', function() { 
+    socketServer.emit('message', JSON.stringify({count: socketServer.engine.clientsCount}));
+  });
+  socketServer.emit('message', JSON.stringify({count: socketServer.engine.clientsCount}));
   socketClient.on('message', async (json) => {
-    if (json === 'null') {
-        socketServer.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify({count: socketServer.clients.size}));
-            }
-        });
-        return;
-    }
-    const { message, user_id } = JSON.parse(json);
+    console.log(json);
+    const { user_id, message } = JSON.parse(json);
     if (message !== "null") {
-        socketServer.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify({message: message, user_id: user_id}));
-            }
-        });
+      socketServer.emit('message', JSON.stringify({message: message, user_id: user_id}));
     } else {
         const random_file_name = files[Math.floor(Math.random() * files.length)];
         const { data } = await getGif();
-        socketServer.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({audio: random_file_name, gif: data.image_url, user_id: user_id}));
-            }
-        });
+        socketServer.emit('message', JSON.stringify({audio: random_file_name, gif: data.image_url, user_id: user_id}));
     }
-  });
-
-  socketClient.on('close', (socketClient) => {
-    socketServer.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({count: socketServer.clients.size}));
-        }
-    });
   });
 });
